53c3e71d2e1d9aafe80a175c68586044a79ff193
nsamedaisy <nsamedaisy@gmail.com>
2023-08-17 16:21:16 +0100

    scritp.js implemented and working

---
+++ script.js
@@ -0,0 +1,35 @@
+let display = document.getElementById("display");
+let equation = "";
+let isOperatorClicked = false;
+
+function appendValue(value) {
+  equation += value;
+  display.value = equation;
+  isOperatorClicked = false;
+}
+
+function appendOperator(operator) {
+  equation += operator;
+  isOperatorClicked = true;
+}
+
+function clearDisplay() {
+  equation = ""
+  display.value = ""
+}
+
+function deleteLastCharacter() {
+  equation = equation.slice(0, -1)
+  display.value = equation
+}
+
+function CalculateResult() {
+  try {
+    equation = equation.replace(/%/g, "*0.01");
+    let result = eval(equation);
+    equation = result.toString();
+    display.value = equation;
+  } catch (err) {
+    display.value = "Error";
+  }
+}
