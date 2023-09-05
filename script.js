bfcaf509be368f80d3ca953e39e6a144c8d42216
nsamedaisy <nsamedaisy@gmail.com>
2023-09-05 15:47:05 +0100

    fixed linters

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const display = document.getElementById("display");
-let equation = "";
-let isOperatorClicked = false;
+const display = document.getElementById('display')
+let equation = ''
+let isOperatorClicked = false
 
 function appendValue(value) {
-  equation += value;
-  display.value = equation;
-  isOperatorClicked = false;
+  equation += value
+  display.value = equation
+  isOperatorClicked = false
 }
 
 function appendOperator(operator) {
-  equation += operator;
-  isOperatorClicked = true;
+  equation += operator
+  isOperatorClicked = true
 }
 
 function clearDisplay() {
-  equation = "";
-  display.value = "";
+  equation = ''
+  display.value = ''
 }
 
 function deleteLastCharacter() {
-  equation = equation.slice(0, -1);
-  display.value = equation;
+  equation = equation.slice(0, -1)
+  display.value = equation
 }
 
 function CalculateResult() {
   try {
-    equation = equation.replace(/%/g, "*0.01");
-    let result = eval(equation);
-    equation = result.toString();
-    display.value = equation;
+    equation = equation.replace(/%/g, '*0.01')
+    let result = eval(equation)
+    equation = result.toString()
+    display.value = equation
   } catch (err) {
-    display.value = "Error";
+    display.value = 'Error'
   }
 }
