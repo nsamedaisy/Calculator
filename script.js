984d5a706873ce142ab08f8305af682406e8902a
nsamedaisy <nsamedaisy@gmail.com>
2023-09-18 15:18:37 +0100

    fixed linters error

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-const display = document.getElementById('display')
-let equation = ''
-let lastInputWasOperator = false
+const display = document.getElementById("display");
+let equation = "";
+let lastInputWasOperator = false;
 
-function appendValue (value) {
-  equation += value
-  display.value = equation
-  lastInputWasOperator = false
+function appendValue(value) {
+  equation += value;
+  display.value = equation;
+  lastInputWasOperator = false;
 }
 
-function appendOperator (operator) {
+function appendOperator(operator) {
   if (!lastInputWasOperator) {
-    equation += operator
-    display.value = equation
-    lastInputWasOperator = true
+    equation += operator;
+    display.value = equation;
+    lastInputWasOperator = true;
   }
 }
 
-function clearDisplay () {
-  equation = ''
-  display.value = ''
-  lastInputWasOperator = false
+function clearDisplay() {
+  equation = "";
+  display.value = "";
+  lastInputWasOperator = false;
 }
 
-function deleteLastCharacter () { 
-  equation = equation.slice(0, -1)
-  display.value = equation
-  lastInputWasOperator = (equation.slice(-1) in {'+': true, '-': true, '*': true, '/': true}) 
+function deleteLastCharacter() {
+  equation = equation.slice(0, -1);
+  display.value = equation;
+  lastInputWasOperator =
+    equation.slice(-1) in { "+": true, "-": true, "*": true, "/": true };
 }
 
-function calculateResult () {
+function calculateResult() {
   try {
-    equation = equation.replace(/%/g, '*0.01')
-    const result = eval(equation)
-    equation = result.toString()
-    display.value = equation
-    lastInputWasOperator = false
+    equation = equation.replace(/%/g, "*0.01");
+    const result = Function(`'use strict'; return (${equation})`)();
+    equation = result.toString();
+    display.value = equation;
+    lastInputWasOperator = false;
   } catch (err) {
-    display.value = 'Error'
+    display.value = "Error";
   }
 }
